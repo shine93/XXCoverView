609432fef6c91a55bc830e85e63776ebf19ccde5
mengxue.wei <1834093885@qq.com>
2018-07-04 16:07:34 +0800

    Initial Commit

---
+++ XXCoverView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XXCoverView
+//
+//  Created by 人人 on 2018/7/4.
+//  Copyright © 2018年 人人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
